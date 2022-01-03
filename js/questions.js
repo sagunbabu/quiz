3e07d55fa4131a23aab6051c4c64831b7d2b7161
Sagun Babu Adhikari <sagunbabu@yahoo.com>
2022-01-03 11:56:29 +0530

    commit1

---
+++ js/questions.js
@@ -0,0 +1,90 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The property in CSS used to change the background color of an element is",
+    answer: "background-color",
+    options: [
+      "color",
+      "bgcolor",
+      "background-color",
+      "background-color : blue;"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The CSS property used to make the text bold is",
+    answer: "font-weight : bold",
+    options: [
+      "style: bold;",
+      "weight: bold;",
+      "font: bold;",
+      "font-weight : bold;"
+    ]
+  },
+  // {
+  //   numb: ,
+  //   question: "",
+  //   answer: "",
+  //   options: [
+  //     "",
+  //     "",
+  //     "",
+  //     ""
+  //   ]
+  // },
+];
